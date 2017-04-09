400d8658845e0ff7892dfd920a58f8b7f532ae92
Sergio Mendoza <smendoza787@gmail.com>
2017-04-09 01:35:54 -0700

    added CLR tables to commandline class

--- lib/scoreboard_cli_gem/commandline.rb
+++ lib/scoreboard_cli_gem/commandline.rb
@@ -1,4 +1,6 @@
 class CommandLine
+  include CommandLineReporter
+
   def call
     puts "Hello, Welcome to the NBA Today Gem!"
     puts "Type in 'nba today' for the latest NBA scores or type 'exit' to quit!"
@@ -14,6 +16,7 @@ class CommandLine
       case input
       when "nba today"
         show_games_table
+        pick_game_menu
       else
         puts "Please type in 'nba today' or 'exit'!"
         print "> "
@@ -23,7 +26,35 @@ class CommandLine
   end
 
   def show_games_table
-    scores = Scraper.new.scrape_nba_scores
-    binding.pry
+    games = Scraper.new.scrape_nba_scores
+
+    games.each_with_index do |game, index|
+      table(:border => true) do
+        row do
+          column('GAME NO.', :width => 5)
+          column('TEAM', :width => 30, :align => 'center')
+          column('SCORE', :width => 10, :align => 'right')
+        end
+        row do
+          column(index)
+          column(game.away_team.name)
+          column(game.away_team.score[0])
+        end
+        row do
+          column('')
+          column(game.home_team.name)
+          column(game.home_team.score[0])
+        end
+      end
+    end
+  end
+
+  def pick_game_menu
+    puts "Choose a GAME NO. to view more stats and info"
+
+    print "> "
+    input = gets.strip
+
+
   end
 end
